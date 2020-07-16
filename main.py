46f629a3d6ad78331ae7e6dc973a6924165353a9
Nikil Reddy <44868474+nikhilreddybilla28@users.noreply.github.com>
2020-07-16 20:02:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 #!pip install streamlit
-from fasti.vision import *
+from fastai.vision import *
 from torch.jit.annotations import Optional
 import pickle
 import streamlit as st
