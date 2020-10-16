f9c9a9943d8c7fe0443b303b881eb1c5098e6142
Nikil Reddy <44868474+nikhilreddybilla28@users.noreply.github.com>
2020-10-16 10:40:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import torchvision.transforms as T
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("PET BREED CLASSIFIER")
 
+introduction_str = 'This is a pet breed classifier.  It was made with 7349 images corresponding to 37 breeds, taken from The Oxford-IIIT Pet Dataset. It was done by transfer of learning using the resnet34 model from Pytorch and the Fastai libraries. \
+    In the validation of the model, the training error was 5%.'
+
+st.markdown(introduction_str)
+
 #load model
 defaults.device = torch.device('cpu')
 
@@ -21,31 +26,32 @@ learner = load_learner(path, "model.pkl")
 
 def classifybreed(img):
   pred_class, pred_idx, outputs = learner.predict(img)
-  st.write(pred_class)
-  return pred_class 
+  #st.write(pred_class)
+  return pred_class ,pred_idx, outputs
 
-def pil2fast(img):
-  return Image(T.ToTensor()(img))
 
 def main():
     html_temp = """
-    <div style="background-color:tomato;padding:12px">
-    <h2 style="color:white;text-align:center;"> Bredd classifier App </h2>
+    <div style="background-color:black;padding:10px">
+    <h2 style="color:white;text-align:center;"> pet breed classifier </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
     img_bytes = st.file_uploader("Squash It!!", type=['png', 'jpg', 'jpeg'])
     if img_bytes is not None:
+        st.image(img_bytes, width=150)
         st.write("Image Uploaded Successfully:")
         img = open_image(img_bytes)
         #img_pil = PIL.Image.open(img_bytes)
         #img = pil2fast(img_pil)
         #img = open_image(BytesIO(img_bytes))
 
-        if st.button("Predict"):
+        if st.button(""):
             st.write("Classifying...")
-            result = classifybreed(img)
-            st.success('The output is {}'.format(result))
+            pred_class ,pred_idx, outputs = classifybreed(img)
+            out_label = f'Prediction: {pred_class};\n\n Probability: {outputs[pred_idx]:.03f}'
+            st.success(out_label)
+            #st.success('The output is {}'.format(result))
 
 if __name__=='__main__':
     main()
